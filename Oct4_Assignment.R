922357308dc893eab273147486d7f6b176fdef54
AjayKk27 <ak834@bu.edu>
2023-10-04 15:29:59 -0400

    Wrote function to remove commas and retrieve distinct footnotes

--- Oct4_Assignment.R
+++ Oct4_Assignment.R
@@ -8,3 +8,9 @@ strwb_census <- strawberry |> filter(Program=="CENSUS")
 strwb_survey<- strawberry |> filter(Program=="SURVEY")
 
 strwb_census$Value<- as.numeric(str_replace_all(strwb_census$Value,",",""))
+
+uncomma<- function(var){
+  dist_footnotes <- unique(var[which(is.na(as.numeric(var)))])
+  return(list(str_replace_all(var,",",""), dist_footnotes))
+}
+uncomma(strwb_census$Value)
